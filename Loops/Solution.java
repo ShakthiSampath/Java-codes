8cbaa835868b6b1491cbfa9fdb3c479ccb2c409b
Shakthi <shakthivvbhss@gmail.com>
2016-10-16 12:19:58 +0530

    Final edit Solution.java file
    Added comments.

--- Loops/Solution.java
+++ Loops/Solution.java
@@ -7,11 +7,11 @@ import java.util.regex.*;
 public class Solution {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in); // Scanner class used which belongs to util package for input
         int n = in.nextInt();
         for(int i=1;i<=10;i++){
         int j=n*i;
-            System.out.println(n+" x "+i+" = "+j);
+            System.out.println(n+" x "+i+" = "+j);  // '+' Symbol helps in concatenation of the values of the variables.
         }
     }
 }
