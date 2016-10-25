ff09b72d46edaa2a8d2ef637c0c801672fc60b3d
Shakthi <shakthivvbhss@gmail.com>
2016-10-25 18:11:08 +0530

    Wip - Solution.java

--- Strings&Loops/Solution.java
+++ Strings&Loops/Solution.java
@@ -5,13 +5,23 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String S = sc.next();
-        String S1=new String();
-        String S2=new String();
-
-        for(int i=0;i<S.length();i++)
+        int n = sc.nextInt();
+        for(int a = 0; a < n; a++)
         {
 
+          String S = sc.next();
+          for(int i=0;i<S.length();i=i+2)
+            {
+              System.out.print(S.charAt(i));
+            }
+
+          for(int j=1;j<S.length();j=j+2)
+            {
+              System.out.print("  ");
+              System.out.print(S.charAt(j));
+            }
+
         }
+
     }
 }
