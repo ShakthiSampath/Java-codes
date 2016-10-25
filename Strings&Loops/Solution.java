6562e7ece534fad6cbfeeacadc53caed540c0f45
Shakthi <shakthivvbhss@gmail.com>
2016-10-25 12:08:36 +0530

    Wip- Solution.java
    In process coding

--- Strings&Loops/Solution.java
+++ Strings&Loops/Solution.java
@@ -4,6 +4,27 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        String S = sc.next();
+        for(int i=0;i<strlen;i++)
+        {
+          if(i%2!=0)
+          {
+            String S1 = sc.next();
+            for(int i =0;i<strlen;i++)
+            {
+              S1= S[i]
+            }
+          }
+          else
+          {
+            String S2 = sc.next();
+            for(int i =0;i<strlen;i++)
+            {
+              S2= S[i]
+            }
+          }
+        }
+        System.out.println(S1+"  "+S2);
     }
 }
