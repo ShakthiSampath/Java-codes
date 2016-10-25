2d713fa41bd7456afdd6fbb66cbbb60606b14cd6
Shakthi <shakthivvbhss@gmail.com>
2016-10-25 12:45:03 +0530

    Wip - Solution.md

--- Strings&Loops/Solution.java
+++ Strings&Loops/Solution.java
@@ -6,8 +6,8 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String S = sc.next();
-        String S1 = sc.next();
-        String S2 = sc.next();
+        String S1=new String();
+        String S2=new String();
 
         for(int i=0;i<S.length();i++)
         {
