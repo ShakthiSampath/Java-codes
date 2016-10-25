e9cb6af93b2f8fd76e77b5bbe488e12ae0e39323
Shakthi <shakthivvbhss@gmail.com>
2016-10-25 18:26:14 +0530

    Final Solution.java
    Added Comments

--- Strings&Loops/Solution.java
+++ Strings&Loops/Solution.java
@@ -5,16 +5,16 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();  
+        int n = sc.nextInt();   // n is the number of input strings needed
         for(int a = 0; a < n; a++)
         {
 
-          String S = sc.next();
+          String S = sc.next();      // new string created
           for(int i=0;i<S.length();i=i+2)
             {
-              System.out.print(S.charAt(i));
+              System.out.print(S.charAt(i));    // charAt helps in handling characters individually from a string or array 
             }
-            System.out.print("  ");
+            System.out.print("  ");    // to create 2 space-separated strings
           for(int j=1;j<S.length();j=j+2)
             {
               System.out.print(S.charAt(j));
