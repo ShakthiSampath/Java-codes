f488acc89b6ad9965a1f0445afcf1a1d51bb5cbc
Shakthi <shakthivvbhss@gmail.com>
2016-10-27 20:39:50 +0530

    Wip - Solution.java

--- Arrays/Solution.java
+++ Arrays/Solution.java
@@ -11,6 +11,10 @@ public class Solution {
         for(int i=0; i < n; i++){
             arr[i] = in.nextInt();
         }
+
+        for(int j=n-1;j<=0;j--){
+            System.out.print(arr[j]+" ");
+        }
         in.close();
     }
 }
