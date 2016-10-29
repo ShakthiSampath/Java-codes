3f276c2217701e3ed6c502f119e64f2ad742a349
Shakthi <shakthivvbhss@gmail.com>
2016-10-29 14:15:38 +0530

    Final edit - Solution.java
    Added Comments

--- Arrays/Solution.java
+++ Arrays/Solution.java
@@ -7,18 +7,16 @@ public class Solution {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int[] arr = new int[n];
+        int[] arr = new int[n];     // initializing an array
         for(int i=0; i < n; i++){
-            arr[i] = in.nextInt();
+            arr[i] = in.nextInt();    // keyboard input value
         }
 
-      //  int
-        //int[] arr1= new int[n];
         for( int j=n-1;j>-1;j--)
         {
 
             System.out.print(arr[j]);
-            System.out.print(" ");
+            System.out.print(" ");   // space-separated value
         }
         in.close();
     }
