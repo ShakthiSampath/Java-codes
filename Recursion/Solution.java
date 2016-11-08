7703c41b7a43a8c86ce320201d3e0c76795f14e2
Shakthi <shakthivvbhss@gmail.com>
2016-11-08 18:52:43 +0530

    Final Solution.java
    Factorial using Recursion

--- Recursion/Solution.java
+++ Recursion/Solution.java
@@ -2,20 +2,19 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-    int factorial(int a)
+    static int factorial(int a)
     {
-      if(a==0)
-        return 0;
-      else
-      {
-        factorial(a)=a*factorial(a-1);
-        return a;
-      }
+      int res;
+      if(a==1)
+       return 1;
+
+      res = a*factorial(a-1);
+      return res;
     }
     public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
       int recur_Var = sc.nextInt();
-      int res_Recursion = factorial(recur_Var);
-      System.out.println(res_Recursion);
+      //int res_Recursion = factorial(recur_Var);
+      System.out.println(factorial(recur_Var));
     }
 }
