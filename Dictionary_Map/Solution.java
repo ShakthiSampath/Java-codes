e095c53c2b7ce143026fcd0727fd6425cd88d4ed
Shakthi <shakthivvbhss@gmail.com>
2016-11-16 17:35:44 +0530

    Wip - Solution.java

--- Dictionary_Map/Solution.java
+++ Dictionary_Map/Solution.java
@@ -9,26 +9,27 @@ class Solution{
         int phone;
         int n = in.nextInt();
         Map <String,Integer> phoneBook = new HashMap <String,Integer>();
-        for(int i = 0; i < n; i++){
+        for(int i = 1; i < n+1; i++){
             name = in.next();
             phone = in.nextInt();
             phoneBook.put(name,phone);
         }
         while(in.hasNext()){
             String s = in.next();
-            for(String key :phoneBook.keySet()){
-            if(key.equals(s))
+            //Map <String,Integer> key = new HashMap <String,Integer>();
+            for(int i=0;i<phoneBook.size();i++){
+            key=phoneBook.keySet();
+            if(s.equals(key))
             {
               System.out.println(s+"="+phoneBook.get(s));
-              break;
             }
             else
             {
-              System.out.println("Not found");
-              break;
-            }
+                System.out.println("Not found");
             }
           }
-        in.close();
+          }
+    in.close();
     }
+
 }
