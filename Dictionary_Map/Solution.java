ee9c28071b5af41252f59e883ea404628b9a564a
Shakthi <shakthivvbhss@gmail.com>
2016-11-17 13:18:45 +0530

    Added comments to Dictionaries_Maps Solution.java
    Comments are good for understanding programs!

--- Dictionary_Map/Solution.java
+++ Dictionary_Map/Solution.java
@@ -5,21 +5,19 @@ import java.io.*;
 class Solution{
     public static void main(String []argh){
         Scanner in = new Scanner(System.in);
-        String name;
-        int phone;
         int n = in.nextInt();
-        Map <String,Integer> phoneBook = new HashMap <String,Integer>();
+        Map <String,Integer> phoneBook = new HashMap <String,Integer>();           // Map DataStructure used - HashMap
         for(int i = 1; i < n+1; i++){
-            name = in.next();
-            phone = in.nextInt();
-            phoneBook.put(name,phone);
+            String name = in.next();
+            int phone = in.nextInt();
+            phoneBook.put(name,phone);                                            // name and phone used in phoneBook Dictionary
         }
         while(in.hasNext()){
             String s = in.next();
             for (int i = 0; i < phoneBook.size(); i++) {
-            if(phoneBook.get(s)!=null){
+            if(phoneBook.get(s)!=null){                                           // checking whether the value of String s in phoneBook is null or not
               System.out.println(s+"="+phoneBook.get(s));
-              break;}
+              break;}                                                             // break statement to break the loop
             else{
               System.out.println("Not found");
               break;}
