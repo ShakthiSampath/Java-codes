ba2afb7631efbe7c81e3961aad934c1922b30d19
Shakthi <shakthivvbhss@gmail.com>
2016-11-21 13:10:24 +0530

    Wip - Solution.java
    Some bug

--- DecToBinary/Solution.java
+++ DecToBinary/Solution.java
@@ -4,7 +4,22 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-      
-
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      int arr_bin[] = new int[20];
+      int rem[] = new int[20];
+      int count=0;
+      for(int i=0;i<rem.length;i++)
+      {
+        while(n!=0)
+        {
+          n=n/2;
+          rem[i]=n%2;
+          System.out.println(rem[i]);
+          if(rem[i]==1)
+            count++;
+        }
+      }
+      System.out.println(count);
     }
 }
