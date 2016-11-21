8a26e74d9973560ebad3735f48dbe01c85d49013
Shakthi <shakthivvbhss@gmail.com>
2016-11-21 14:00:33 +0530

    Final Solution.java
    Added Comments

--- DecToBinary/Solution.java
+++ DecToBinary/Solution.java
@@ -4,10 +4,9 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);
+      Scanner sc = new Scanner(System.in);  // creating Scanner class for input
       int n = sc.nextInt();
-      int arr_bin[] = new int[20];
-      int rem[] = new int[20];
+      int rem[] = new int[20];  // using array to store remainder
       int count=0;
       for(int i=0;i<rem.length;i++)
       {
@@ -15,9 +14,9 @@ public class Solution {
         {
           rem[i]=n%2;
           n=n/2;
-          System.out.println(rem[i]);
+          System.out.println(rem[i]); // printing binary numbers
           if(rem[i]==1)
-            count++;
+            count++;                  // counting the number of 1's
         }
       }
       System.out.println(count);
