98818f745897098abb11667b630bea984ff81ac9
Shakthi <shakthivvbhss@gmail.com>
2016-11-26 10:29:42 +0530

    Final Solution2.java
    Finished coding!

--- DecToBinary/Solution2.java
+++ DecToBinary/Solution2.java
@@ -4,32 +4,29 @@ import java.util.*;
 public class Solution2 {
 
     public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);  // creating Scanner class for input
+      Scanner sc = new Scanner(System.in);  
       int n = sc.nextInt();
       int rem[] = new int[100];
-      //String rem1= new String(Integer.toBinaryString(n));  // using array to store remainder
       int count=0,count1=0,index=0;
       while(n>0){
-          rem[index++]=n%2;                      // calculating remainder
+          rem[index++]=n%2;
           n=n/2;
         }
 
-      for(int j=rem.length-1;j>=0;){
+      for(int j=0;j<index;j++){
         System.out.print(rem[j]);
         if(rem[j]==1){
-          while(rem[j]==1){
-            j--;
             count1++;
-          }
-          count=count1;
+            if(count1>count)
+            {
+                count=count1;
+            }
         }
         else{
-          j--;
           count1=0;
-          continue;
         }
       }
-      //System.out.println();
+      System.out.println();
       System.out.println(count);
     }
 }
