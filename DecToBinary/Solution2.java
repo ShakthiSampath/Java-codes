8d384ea35cc28fde13ae78c449428e4876317ae6
Shakthi <shakthivvbhss@gmail.com>
2016-11-26 10:33:19 +0530

    Final Solution2.java - added Comments
    Small changes in the code and added comments.

--- DecToBinary/Solution2.java
+++ DecToBinary/Solution2.java
@@ -4,22 +4,22 @@ import java.util.*;
 public class Solution2 {
 
     public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);  
+      Scanner sc = new Scanner(System.in);  // creating Scanner class for input
       int n = sc.nextInt();
-      int rem[] = new int[100];
+      int rem[] = new int[100];     // using array to store remainder
       int count=0,count1=0,index=0;
       while(n>0){
-          rem[index++]=n%2;
+          rem[index++]=n%2;         // calculating remainder
           n=n/2;
         }
 
-      for(int j=0;j<index;j++){
-        System.out.print(rem[j]);
+      for(int j=index-1;j>=0;j--){
+        System.out.print(rem[j]);     // printing out the remainder
         if(rem[j]==1){
             count1++;
             if(count1>count)
             {
-                count=count1;
+                count=count1;         // using a count variable
             }
         }
         else{
@@ -27,6 +27,6 @@ public class Solution2 {
         }
       }
       System.out.println();
-      System.out.println(count);
+      System.out.println(count);      // printing the count variable
     }
 }
