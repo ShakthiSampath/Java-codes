eb465aaebe4aba692bebca63ddc61f70afce17dc
Shakthi <shakthivvbhss@gmail.com>
2016-12-20 13:04:53 +0530

    Wip Solution.java

--- Inheritance/Solution.java
+++ Inheritance/Solution.java
@@ -24,10 +24,39 @@ class Student extends Person{
 
   //Constructor
   Student(String firstName, String lastName, int idNumber, int[] scores){
-    
+    this.firstName=firstName;
+    this.lastName=lastName;
+    this.idNumber=idNumber;
+    this.testScores=scores;
   }
 
-}
+  // calculate method
+  char calculate(){
+    for(int i=0;i<numScores;i++)
+    {
+      int sum = 0;
+      sum=sum+testScores[i];
+    }
+    int avg;
+    avg = sum/numScores;
+
+    // Calculating Grade
+    if(avg<=100 && avg>=90)
+      return 'O';
+    else if(avg<=90 && avg>=80)
+      return 'E';
+    else if(avg<=80 && avg>=70)
+      return 'A';
+    else if(avg<=70 && avg>=55)
+      return 'P';
+    else if(avg<=55 && avg>=40)
+      return 'D';
+    else
+      return 'T';
+
+  }
+
+};
 
 class Solution {
 	public static void main(String[] args) {
