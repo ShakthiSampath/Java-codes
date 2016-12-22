2edf6c871b9d41a106a89d2f2f0ef50c5d3981ef
Shakthi <shakthivvbhss@gmail.com>
2016-12-22 17:52:18 +0530

    Wip Solution.java

--- Output_Formatting/Solution.java
+++ Output_Formatting/Solution.java
@@ -1,1 +1,9 @@
 import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            String[] str = new String[3];
+            String[] num = new String[3];
+            int[] len = new int[3];
