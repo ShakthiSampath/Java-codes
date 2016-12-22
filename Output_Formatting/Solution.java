367e5749e77d511558b2c2e1649219972318c64b
Shakthi <shakthivvbhss@gmail.com>
2016-12-22 17:53:28 +0530

    Wip Solution.java
    Input Formatting done.

--- Output_Formatting/Solution.java
+++ Output_Formatting/Solution.java
@@ -7,3 +7,18 @@ public class Solution {
             String[] str = new String[3];
             String[] num = new String[3];
             int[] len = new int[3];
+
+            for(int i=0;i<3;i++)
+            {
+                String s1 = sc.next();
+                String x = sc.next();
+                str[i] = s1;
+                len[i]=15-s1.length();
+
+                if(x.length()==3)
+                  num[i]=x;
+                else if(x.length()==2)
+                  num[i]="0"+x;
+                else if(x.length()==1)
+                  num[i]="00"+x;
+            }
