c2ab3ce8b9b3e06511accd580b48e650cf47e7f1
Shakthi <shakthivvbhss@gmail.com>
2016-12-23 22:47:05 +0530

    Added Comments to Solution.java

--- Abstract_Class/Solution.java
+++ Abstract_Class/Solution.java
@@ -1,29 +1,29 @@
 import java.util.*;
 
-abstract class Book
+abstract class Book             // created abstract class
 {
     String title;
     String author;
-    Book(String t,String a){
+    Book(String t,String a){    // base constructor having title and author
         title=t;
         author=a;
     }
-    abstract void display();
-
+    abstract void display();   /* abstract class contain abstract methods
+                                  that is without any body. */
 }
 
-class MyBook extends Book
+class MyBook extends Book      // derived class MyBook inheriting base class
 {
     int price;
     MyBook(String title,String author,int price){
 
-      super(title,author);
-      this.price = price;
+      super(title,author);        // derived constructor using base
+      this.price = price;         // constructor's variables
 
     }
 
-    void display(){
-
+    void display(){               // body of display method written
+                                  // in derived class
       System.out.println("Title: "+title);
       System.out.println("Author: "+author);
       System.out.println("Price: "+price);
@@ -40,8 +40,8 @@ public class Solution
       String title=sc.nextLine();
       String author=sc.nextLine();
       int price=sc.nextInt();
-      Book new_novel=new MyBook(title,author,price);
-      new_novel.display();
-
+      Book new_novel=new MyBook(title,author,price); // Implementing above
+      new_novel.display();                           // class's method
+                          
    }
 }
