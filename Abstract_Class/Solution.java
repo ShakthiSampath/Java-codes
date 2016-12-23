5f233f3547d3b8f39a1110d74bce85c509819b72
Shakthi <shakthivvbhss@gmail.com>
2016-12-23 21:57:49 +0530

    Wip Solution.java

--- Abstract_Class/Solution.java
+++ Abstract_Class/Solution.java
@@ -1,1 +1,14 @@
 import java.util.*;
+
+abstract class Book
+{
+    String title;
+    String author;
+    Book(String t,String a){
+        title=t;
+        author=a;
+    }
+    abstract void display();
+
+
+}
